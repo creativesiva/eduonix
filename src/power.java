fd22510fb1f9c386acdd947695130818adc820d3
vjonnalagadda <vjonnalagadda@AIPL-DW7P542.avineonindia.com>
2016-11-10 17:28:52 +0530

    power

---
+++ src/power.java
@@ -0,0 +1,19 @@
+
+public class power {
+	public static void main(String args[]) {
+		int a = 2;
+		int b = 3;
+		int x = 0;
+		int y = 0;
+		int p = 0;
+		int q = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+			p += i;
+
+		}
+		System.out.println(p);
+	}
+
+}
