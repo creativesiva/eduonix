fb4143cb2e16f6f07f74a834f93b1c673762bec7
vjonnalagadda <vjonnalagadda@AIPL-DW7P542.avineonindia.com>
2016-11-10 18:38:17 +0530

    A_Premitives

---
+++ src/eduonix/A_Premitives.java
@@ -0,0 +1,19 @@
+package eduonix;
+
+public class A_Premitives {
+
+	public static void main(String[] args) {
+		int number = -10;
+		double decimal = 20.25;
+		int sum = (int)(number + decimal);
+		boolean myBool = true;
+		System.out.println(sum);
+		if (myBool){
+			System.out.println("I am siva prasad");
+		}else{
+			System.out.println("Who am I?");
+		}
+	}
+	
+
+}
